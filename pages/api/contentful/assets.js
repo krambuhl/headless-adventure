e7b7a19826cee7446067b0197a4cf8697d2b8a44
Eevee Kreevee <thekrambuhl@gmail.com>
2020-05-07 16:01:35 -0700

    wrap and parse contentful api for application

---
+++ pages/api/contentful/assets.js
@@ -0,0 +1,18 @@
+import { drafts, published } from '@lib/contentful/clients'
+import parseEntry from '@lib/contentful/parseEntry'
+
+export default (req, res) => {
+  const { preview } = req.query
+  const client = preview !== undefined ? drafts : published
+
+  client.getAssets()
+    .then(response => {
+      res.json(response.items.map(entry => parseEntry(entry)))
+    })
+    .catch(err => {
+      if (err.sys) {
+        res.json(err.sys)
+      }
+    })
+    .finally(() => res.end())
+}
---
+++ pages/api/contentful/entry/[id].js
@@ -0,0 +1,17 @@
+import { drafts, published } from '@lib/contentful/clients'
+import parseEntry from '@lib/contentful/parseEntry'
+
+export default (req, res) => {
+  const { id, preview } = req.query
+  const client = preview !== undefined ? drafts : published
+
+  client.getEntry(id)
+    .then(response => res.json(parseEntry(response)))
+    .catch(err => {
+      if (err.sys) {
+        res.json(err.sys)
+      }
+    })
+    .finally(() => res.end())
+}
+
