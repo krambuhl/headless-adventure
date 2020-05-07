66497c301eb41836931cb0ed9e35c16d99ebae3c
Eevee Kreevee <thekrambuhl@gmail.com>
2020-05-07 15:24:20 -0700

    create contentful api endpoints

---
+++ pages/api/contentful/draft.js
@@ -0,0 +1,14 @@
+import createClient from '@lib/contentful/createClient'
+import parseEntry from '@lib/contentful/parseEntry'
+
+const client = createClient({ isPreview: true })
+
+export default (req, res) => {
+  client.getEntries()
+    .then(response => {
+      res.json(response.items.map(entry => parseEntry(entry)))
+    })
+    .catch(console.error)
+    .finally(() => res.end())
+
+}
